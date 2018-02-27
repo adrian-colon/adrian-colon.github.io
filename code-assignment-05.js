dab909ae9e2ea74e01bd9cacfa37898a63edf552
adrian-colon <acolon5@lsu.edu>
2018-02-26 18:09:57 -0600

    added code 4 and code 5 files

---
+++ code-assignment-05.js
@@ -0,0 +1,9 @@
+let myNumber = 7;
+let myString = "This is my string.";
+let myBoolean = true;
+let myArray = ['apple', 'orange', 'pear'];
+
+console.log("My number is " + myNumber);
+console.log("My string is: \"" + myString + "\"");
+console.log("My boolean is " + myBoolean);
+console.log("My array is " + myArray.toString());
